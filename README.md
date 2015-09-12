4219fca4d394c33e40dd1543b5e62587bef1dd9d
sgnl <rayrfarias@gmail.com>
2015-09-11 20:13:30 -1000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# JS-Basics for Dev League Prep
+
+# References
+- [slides](https://slides.com/sgnl/js-basics)
---
+++ index.js
@@ -0,0 +1,112 @@
+/* create variables */
+
+/**
+* #1
+* Variables with a String as their value
+*
+* declare variables named `first_name`, `last_name`, and `birth_place`.
+* Fill out the values for these with your own data or make it up. It should contain a String
+**/
+
+
+
+/**
+* #2
+* Variables with a Number as their value
+*
+* declare variables named `favorite_number`, `current_year`, and `that_one_prince_song`.
+* Their values should be a Number
+**/
+
+
+
+/**
+* #3
+* Variables with Boolean values
+*
+* declare variables named `isDaytime`, `isLeftHanded`, `inHawaii`, and `isHappyCoding`
+* Their values should be a Boolean
+**/
+
+
+
+/**
+* #4
+* Variables with null
+*
+* declare variables named `enrolledAtDevLeague` and `completedFridayPrep`
+* Their values should be a Null value
+**/
+
+
+/**
+* #5a
+* Variables with an Array for it's value. This Array will contain:
+*
+* declare a variable named `west_coast` which is an Array. This Array contains the names of all the states
+* which can be found along the west coast of The United States.
+**/
+
+
+
+/**
+* #5b
+* Variables with an Array for it's value. This Array will contain:
+*
+* declare a variable named `even numbers` which is an Array. This Array contains numbers that
+* are considered 'even values'. Have at least ten even values in this array
+**/
+
+
+
+/**
+* #6a Arithmetic
+*/
+
+
+
+/**
+* #6b Comparisons & Logic
+*/
+
+
+
+/**
+* #6c Assignments
+*/
+
+
+
+/**
+* #7a
+* Declare Functions
+*
+* declare a Function named `jump` which takes a single argument, `height`
+*
+* this function should return a String along the lines of, "You Jumped 9 feet high!"
+* if invoked in this way -> jump(9);
+*
+* store the return value to a variable and use console.log to inspect the value which was passed back by your function
+**/
+
+
+
+/**
+* #7b
+* Declare Functions
+*
+* declare a Function named `cook` which a takes a three arguments, `ingredient1`, `ingredient2`, `recipeName`
+*
+* this function should return a String along the lines of, "Tomatoes and Cheese make a Pizza!"
+* if invoked in this way -> cook("Tomatoes", "Cheese", "Pizza");
+**/
+
+
+
+/**
+* #7c
+* Declare Functions
+*
+* declare Function named
+* Their values should be a Number
+**/
