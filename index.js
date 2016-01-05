8649e9408c341517c3058793f94a7f75513d2fdb
Jay Won <jason@sudokrew.com>
2016-01-04 17:50:05 -1000

    Documentation revisions.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 * #1
 * Variables with a String as their value
 *
-* declare variables named `first_name`, `last_name`, and `birth_place`.
+* Declare variables named `firstName`, `lastName`, and `birthPlace`.
 * Fill out the values for these with your own data or make it up. It should contain a String
 **/
 
@@ -14,7 +14,7 @@
 * #2
 * Variables with a Number as their value
 *
-* declare variables named `favorite_number`, `current_year`, and `that_one_prince_song`.
+* Declare variables named `favoriteNumber`, `currentYear`, and `thatOnePrinceSong`.
 * Their values should be a Number
 **/
 
@@ -24,7 +24,7 @@
 * #3
 * Variables with Boolean values
 *
-* declare variables named `isDaytime`, `isLeftHanded`, `inHawaii`, and `isHappyCoding`
+* Declare variables named `isDaytime`, `isLeftHanded`, `inHawaii`, and `isHappyCoding`
 * Their values should be a Boolean
 **/
 
@@ -34,7 +34,7 @@
 * #4
 * Variables with null
 *
-* declare variables named `enrolledAtDevLeague` and `completedFridayPrep`
+* Declare variables named `enrolledAtDevLeague` and `completedFridayPrep`
 * Their values should be a Null value
 **/
 
@@ -43,7 +43,7 @@
 * #5a
 * Variables with an Array for it's value. This Array will contain:
 *
-* declare a variable named `west_coast` which is an Array. This Array contains the names of all the states
+* Declare a variable named `westCoast` which is an Array. This Array contains the names of all the states
 * which can be found along the west coast of The United States.
 **/
 
@@ -53,26 +53,47 @@
 * #5b
 * Variables with an Array for it's value. This Array will contain:
 *
-* declare a variable named `even numbers` which is an Array. This Array contains numbers that
-* are considered 'even values'. Have at least ten even values in this array
+* Declare a variable named `evenNumbers` which is an Array. This Array contains numbers that
+* are considered 'even values'. Have at least ten even values in this array.
 **/
 
 
 
 /**
 * #6a Arithmetic
+* Variables with the outcome of an arithmetic operation:
+* 
+* Declare a variable named `sumOfNumbers` which is a Number. This Number contains the result of performing an arithmetic
+* operation on two numbers. Have this variable store the result of adding 3 numbers of your choosing.
+*
+* Extend this behavior to include subtraction, multiplication and division. 
 */
+
 
 
 
 /**
 * #6b Comparisons & Logic
+* Variables with the outcome of a comparison operation:
+*
+* Declare a variable named 'isEqual' which is a Boolean. This Boolean contains the result of performing a comparison to
+* test if two strings are equal. We want to compare the two strings "Tacocat" and "tacocat" and console.log() the result.
+*
+* BONUS: See if you can come up with a few examples of double comparison operator(==) vs triple comparison operator(===)
 */
 
 
 
 /**
 * #6c Assignments
+* Variables with the outcome of an assignment operation:
+*
+* Declare a variable named generalContainer. This variable will be used to store multiple types of
+* values as we `assign` them to show how variables in JavaScript are not bound to a single data type
+* and can change values.
+*
+* Write 5 different assignment statements and `console.log()` the value of the variable after each 
+* assignment statement to show the current value.
 */
 
 
@@ -81,12 +102,12 @@
 * #7a
 * Declare Functions
 *
-* declare a Function named `jump` which takes a single argument, `height`
+* Declare a Function named `jump` which takes a single argument, `height`
 *
-* this function should return a String along the lines of, "You Jumped 9 feet high!"
+* This function should return a String along the lines of, "You Jumped 9 feet high!"
 * if invoked in this way -> jump(9);
 *
-* store the return value to a variable and use console.log to inspect the value which was passed back by your function
+* Store the return value to a variable and use console.log to inspect the value which was passed back by your function
 **/
 
 
@@ -95,18 +116,8 @@
 * #7b
 * Declare Functions
 *
-* declare a Function named `cook` which a takes a three arguments, `ingredient1`, `ingredient2`, `recipeName`
+* Declare a Function named `cook` which a takes three arguments/parameters, `ingredient1`, `ingredient2`, `recipeName`
 *
-* this function should return a String along the lines of, "Tomatoes and Cheese make a Pizza!"
+* This function should return a String along the lines of, "Tomatoes and Cheese make a Pizza!"
 * if invoked in this way -> cook("Tomatoes", "Cheese", "Pizza");
-**/
-
-
-
-/**
-* #7c
-* Declare Functions
-*
-* declare Function named
-* Their values should be a Number
 **/
