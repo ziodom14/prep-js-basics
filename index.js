885da9405db3f008b9296079af2b45408ef15f0c
sgnl <rayrfarias@gmail.com>
2016-03-11 13:11:07 -1000

    updates exercise instructions and formatting

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
 * Variables with a String as their value
 *
 * Declare variables named `firstName`, `lastName`, and `birthPlace`.
-* Fill out the values for these with your own data or make it up. It should contain a String
+* Fill out the values for these with your own data or make it up.
+*
+* It should be a String.
 **/
 
 
@@ -14,7 +16,9 @@
 * #2
 * Variables with a Number as their value
 *
-* Declare variables named `favoriteNumber`, `currentYear`, and `thatOnePrinceSong`.
+* Declare variables named `favoriteNumber`, `currentYear`,
+* and `thatOnePrinceSong`.
+*
 * Their values should be a Number
 **/
 
@@ -24,7 +28,9 @@
 * #3
 * Variables with Boolean values
 *
-* Declare variables named `isDaytime`, `isLeftHanded`, `inHawaii`, and `isHappyCoding`
+* Declare variables named `isDaytime`, `isLeftHanded`, `inHawaii`,
+* and `isHappyCoding`
+*
 * Their values should be a Boolean
 **/
 
@@ -35,15 +41,18 @@
 * Variables with null
 *
 * Declare variables named `enrolledAtDevLeague` and `completedFridayPrep`
+*
 * Their values should be a Null value
 **/
+
 
 
 /**
 * #5a
 * Variables with an Array for it's value. This Array will contain:
 *
-* Declare a variable named `westCoast` which is an Array. This Array contains the names of all the states
+* Declare a variable named `westCoast` which is an Array.
+* This Array contains the names of all the states
 * which can be found along the west coast of The United States.
 **/
 
@@ -53,8 +62,9 @@
 * #5b
 * Variables with an Array for it's value. This Array will contain:
 *
-* Declare a variable named `evenNumbers` which is an Array. This Array contains numbers that
-* are considered 'even values'. Have at least ten even values in this array.
+* Declare a variable named `evenNumbers` which is an Array.
+* This Array contains numbers that are considered 'even values'.
+* Have at least ten even values in this array.
 **/
 
 
@@ -62,11 +72,12 @@
 /**
 * #6a Arithmetic
 * Variables with the outcome of an arithmetic operation:
-* 
-* Declare a variable named `sumOfNumbers` which is a Number. This Number contains the result of performing an arithmetic
-* operation on two numbers. Have this variable store the result of adding 3 numbers of your choosing.
 *
-* Extend this behavior to include subtraction, multiplication and division. 
+* Declare a variable named `sumOfNumbers` which is a Number. This Number
+* contains the result of performing an arithmetic operation on two numbers.
+* Have this variable store the result of adding 3 numbers of your choosing.
+*
+* Extend this behavior to include subtraction, multiplication and division.
 */
 
 
@@ -76,10 +87,18 @@
 * #6b Comparisons & Logic
 * Variables with the outcome of a comparison operation:
 *
-* Declare a variable named 'isEqual' which is a Boolean. This Boolean contains the result of performing a comparison to
-* test if two strings are equal. We want to compare the two strings "Tacocat" and "tacocat" and console.log() the result.
+* Declare a variable named `isEqual` which is a Boolean. This Boolean
+* contains the result of performing a comparison to test if two strings
+* are equal. We want to compare the two strings "Tacocat" and "tacocat" and
+* `console.log()` the result.
 *
-* BONUS: See if you can come up with a few examples of double comparison operator(==) vs triple comparison operator(===)
+* NOTE:
+*   There is no test which tests your console.log()'s output'.
+*   Also, there are zero tests for the BONUS below.
+*
+* BONUS:
+*   See if you can come up with a few examples of double comparison
+*   operator(==) vs triple comparison operator(===).
 */
 
 
@@ -88,12 +107,16 @@
 * #6c Assignments
 * Variables with the outcome of an assignment operation:
 *
-* Declare a variable named generalContainer. This variable will be used to store multiple types of
-* values as we `assign` them to show how variables in JavaScript are not bound to a single data type
+* Declare a variable named `generalContainer`. This variable will be used to store multiple types of
+* values as we assign them to show how variables in JavaScript are not bound to a single data type
 * and can change values.
 *
-* Write 5 different assignment statements and `console.log()` the value of the variable after each 
+* Write 5 different assignment statements and `console.log()` the value of the variable after each
 * assignment statement to show the current value.
+*
+* NOTE:
+*   The tests for this section cannot test your variable as it changes,
+*   it can only test that you declared the variable by giving it the right name.
 */
 
 
@@ -104,10 +127,10 @@
 *
 * Declare a Function named `jump` which takes a single argument, `height`
 *
-* This function should return a String along the lines of, "You Jumped 9 feet high!"
+* This function should return a String, "You jumped 9 feet high!"
 * if invoked in this way -> jump(9);
 *
-* Store the return value to a variable and use console.log to inspect the value which was passed back by your function
+* Store the return value to a variable named `jumpResult` and use console.log to inspect the value which was returned by your function
 **/
 
 
@@ -121,3 +144,4 @@
 * This function should return a String along the lines of, "Tomatoes and Cheese make a Pizza!"
 * if invoked in this way -> cook("Tomatoes", "Cheese", "Pizza");
 **/
+
